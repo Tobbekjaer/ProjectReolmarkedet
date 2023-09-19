ed1f3dc92e55f6d9b016cebe870c5ede42dc7494
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-09-19 12:02:27 +0200

    Initiating DisplayBarcodes method

--- ProjectReolmarkedet/PrintBarcodes.xaml.cs
+++ ProjectReolmarkedet/PrintBarcodes.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,5 +25,48 @@ namespace ProjectReolmarkedet
         {
             InitializeComponent();
         }
+
+        public void DisplayBarcodes(string connectionString)
+        {
+
+
+
+            //string ConnectionString = ConfigurationManager.ConnectionStrings["DatabaseServerInstance"].ConnectionString;
+
+            //try {
+            //    using (SqlConnection con = new SqlConnection(ConnectionString)) {
+
+            //        con.Open();
+
+            //        SqlCommand cmd = new SqlCommand("SELECT FlowerName, ProductionTimeInDays, HalfLife, SizeInSquareMeters FROM FlowerSort", con);
+
+            //        using (SqlDataReader reader = cmd.ExecuteReader()) {
+            //            while (reader.Read()) {
+
+            //                string line = "";
+
+            //                foreach (DbDataRecord row in reader) {
+
+            //                    string flowerName = reader["FlowerName"].ToString();
+            //                    int productionTime = int.Parse(reader["ProductionTimeInDays"].ToString());
+            //                    int halfLife = int.Parse(reader["HalfLife"].ToString());
+            //                    double size = double.Parse(reader["SizeInSquareMeters"].ToString());
+
+            //                    line += $"{flowerName}, {productionTime}, {halfLife}, {size}\n";
+
+            //                }
+
+            //                tbSorts.Text = line;
+            //            }
+            //        }
+            //    }
+            //}
+            //catch (Exception ex) {
+            //    MessageBox.Show(ex.Message);
+            //}
+
+        }
+
+
     }
 }
