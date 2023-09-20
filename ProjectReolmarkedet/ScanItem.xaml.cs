87228fe08b3237abf3cff27b5839c07bc2288c95
Sebastian <sebastian99private@icloud.com>
2023-09-20 11:45:25 +0200

    Add products with barcode from database

--- ProjectReolmarkedet/ScanItem.xaml.cs
+++ ProjectReolmarkedet/ScanItem.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,9 +26,49 @@ namespace ProjectReolmarkedet
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        public void DisplaySaleItems(string connectionString)
         {
 
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+
+                    con.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM PRODUCT WHERE ProductID = @ProductID", con);
+                    cmd.Parameters.AddWithValue("@ProductID", tbProductID.Text.Trim());
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        string line = "";
+                        while (reader.Read())
+                        {
+                            string productName = reader["ProductName"].ToString();
+                            int price = Convert.ToInt32(reader["Price"].ToString());
+                            // Skal vi også have employee navn?
+
+                            line += $"Produkt: {productName}, Pris: {price}\n";
+
+                            tbSaleList.Text += line;
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void Scan_Click(object sender, RoutedEventArgs e)
+        {
+            // Configurerer Databasen. husk at bruge de 3 using statements; System.Data; Microsoft.Extensions.Configuration.Json; Microsoft.Extensions.Configuration;
+            IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build(); // Husk at selve json filen skal have navnet appsettings.json
+            string connectionString = config.GetConnectionString("MyDBConnection");
+            DisplaySaleItems(connectionString);
         }
     }
 }
